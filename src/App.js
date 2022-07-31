e7049a089d7de55eb14b6715f132006d54b43165
Claudio Melgaco <claudio19a@hotmail.com>
2022-07-31 15:41:59 -0300

    req 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Form from './components/Form';
+import './components/Form.css';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form />
+        <Form classname="form-box" />
+        <Card />
       </div>
     );
   }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -17,74 +17,92 @@ class Form extends React.Component {
       onInputChange,
       onSaveButtonClick } = this.props;
     return (
-      <form>
-        <input
-          data-testid="name-input"
-          name="name"
-          type="text"
-          value={ cardName }
-          onChange={ onInputChange }
-        />
-        <textarea
-          data-testid="description-input"
-          name="Desc"
-          id=""
-          cols="30"
-          rows="10"
-          value={ cardDescription }
-          onChange={ onInputChange }
-        />
-        <input
-          data-testid="attr1-input"
-          type="number"
-          value={ cardAttr1 }
-          onChange={ onInputChange }
-        />
-        <input
-          data-testid="attr2-input"
-          type="number"
-          value={ cardAttr2 }
-          onChange={ onInputChange }
-        />
-        <input
-          data-testid="attr3-input"
-          type="number"
-          value={ cardAttr3 }
-          onChange={ onInputChange }
-        />
-        <input
-          data-testid="image-input"
-          type="text"
-          placeholder="Insira imagem"
-          value={ cardImage }
-          onChange={ onInputChange }
-        />
-        <select
-          name=""
-          id=""
-          data-testid="rare-input"
-          value={ cardRare }
-          onChange={ onInputChange }
-        >
-          <option value="normal">Normal</option>
-          <option value="raro">Raro</option>
-          <option value="muito raro">Muito raro</option>
-        </select>
-        { hasTrunfo }
-        <input
-          type="checkbox"
-          data-testid="trunfo-input"
-          checked={ cardTrunfo }
-          onChange={ onInputChange }
-        />
-        <button
-          type="submit"
-          data-testid="save-button"
-          onClick={ onSaveButtonClick }
-          disabled={ isSaveButtonDisabled }
-        >
-          Salvar
-        </button>
+      <form className="form-box">
+        <div>
+          <input
+            data-testid="name-input"
+            name="name"
+            type="text"
+            value={ cardName }
+            onChange={ onInputChange }
+          />
+        </div>
+        <div>
+          <textarea
+            data-testid="description-input"
+            name="Desc"
+            id=""
+            cols="30"
+            rows="10"
+            value={ cardDescription }
+            onChange={ onInputChange }
+          />
+        </div>
+        <div>
+          <input
+            data-testid="attr1-input"
+            type="number"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
+          />
+        </div>
+        <div>
+          <input
+            data-testid="attr2-input"
+            type="number"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
+          />
+        </div>
+        <div>
+          <input
+            data-testid="attr3-input"
+            type="number"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
+          />
+        </div>
+        <div>
+          <input
+            data-testid="image-input"
+            type="text"
+            placeholder="Insira imagem"
+            value={ cardImage }
+            onChange={ onInputChange }
+          />
+        </div>
+        <div>
+          <select
+            name=""
+            id=""
+            data-testid="rare-input"
+            value={ cardRare }
+            onChange={ onInputChange }
+          >
+            <option value="normal">Normal</option>
+            <option value="raro">Raro</option>
+            <option value="muito raro">Muito raro</option>
+          </select>
+        </div>
+        <div>
+          { hasTrunfo }
+          <input
+            type="checkbox"
+            data-testid="trunfo-input"
+            checked={ cardTrunfo }
+            onChange={ onInputChange }
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+            data-testid="save-button"
+            onClick={ onSaveButtonClick }
+            disabled={ isSaveButtonDisabled }
+          >
+             Salvar
+          </button>
+        </div>
       </form>
     );
   }
